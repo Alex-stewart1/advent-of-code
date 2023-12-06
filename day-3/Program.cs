40d34894567d86fc2aad5a53ec72d11144ace081
Alex Stewart <alex.stewart@dentalit.ltd.uk>
2023-12-06 23:14:13 +0000

    Advent of code day 3 done

--- day-3/Program.cs
+++ day-3/Program.cs
@@ -2,7 +2,7 @@
 using System.IO.Compression;
 using System.Xml.Serialization;
 
-char[,] engineArray = new char[140, 140];
+char[,] engineArray = new char[140, 140]; //new char[10,10]; 
 
 using var fs = new FileStream(@"D:\git\advent-of-code\day-3\input.txt", FileMode.Open);
 using var sr = new StreamReader(fs);
@@ -11,7 +11,7 @@ var symbolDictionary = new HashSet<char>(){
     '-', '@', '*', '=', '%', '/', '$', '#', '+', '&'
 };
 
-int partNumberSum = 0;
+int gearRatioSum = 0;
 
 for(int i = 0; i < engineArray.GetLength(1); i++){
     var schematicRow = sr.ReadLine();
@@ -25,37 +25,43 @@ for(int i = 0; i < engineArray.GetLength(1); i++){
 
 for(int y = 0; y < engineArray.GetLength(1); y++){
     for(int x = 0; x < engineArray.GetLength(0); x++){
-        if (symbolDictionary.Contains(engineArray[x, y])){
+        if (engineArray[x, y] == '*'){
             //[x-1,y-1],[x,y-1],[x+1,y-1]
             //[x-1,  y]         [x+1,  y]
             //[x-1,y+1],[x,y+1],[x+1,y+1]
 
+            int currentGearCount = 0;
+            int currentGearRatio = 1;
             //TOP
             if(IsCoordInBounds(x-1,y-1)){
-                AddIfNumberAndSetItToX(x-1, y-1);
+                AddIfNumberAndSetItToX(x-1, y-1, ref currentGearCount, ref currentGearRatio);
             }
             if(IsCoordInBounds(x,y-1)){
-                AddIfNumberAndSetItToX(x, y-1);
+                AddIfNumberAndSetItToX(x, y-1, ref currentGearCount, ref currentGearRatio);
             }
             if(IsCoordInBounds(x+1,y-1)){
-                AddIfNumberAndSetItToX(x+1, y-1);
+                AddIfNumberAndSetItToX(x+1, y-1, ref currentGearCount, ref currentGearRatio);
             }
             //MID
             if(IsCoordInBounds(x-1,y)){
-                AddIfNumberAndSetItToX(x-1, y);
+                AddIfNumberAndSetItToX(x-1, y, ref currentGearCount, ref currentGearRatio);
             }
             if(IsCoordInBounds(x+1,y)){
-                AddIfNumberAndSetItToX(x+1, y);
+                AddIfNumberAndSetItToX(x+1, y, ref currentGearCount, ref currentGearRatio);
             }
             //BOTTOM
             if(IsCoordInBounds(x-1,y+1)){
-                AddIfNumberAndSetItToX(x-1, y+1);
+                AddIfNumberAndSetItToX(x-1, y+1, ref currentGearCount, ref currentGearRatio);
             }
             if(IsCoordInBounds(x,y+1)){
-                AddIfNumberAndSetItToX(x, y+1);
+                AddIfNumberAndSetItToX(x, y+1, ref currentGearCount, ref currentGearRatio);
             }
             if(IsCoordInBounds(x+1,y+1)){
-                AddIfNumberAndSetItToX(x+1, y+1);
+                AddIfNumberAndSetItToX(x+1, y+1, ref currentGearCount, ref currentGearRatio);
+            }
+
+            if (currentGearCount == 2){
+                gearRatioSum += currentGearRatio;
             }
         }
     }
@@ -69,26 +75,30 @@ for(int y = 0; y < engineArray.GetLength(1); y++){
     }
     Console.Write("\n");
 }
-
-System.Console.WriteLine($"Part sum: {partNumberSum}");
+System.Console.WriteLine($"Gear ratio sum: {gearRatioSum}");
 
 bool IsCoordInBounds(int x, int y){
     int xBound = engineArray.GetLength(0);
     int yBound = engineArray.GetLength(1);
     return x < xBound && x >= 0 && y < yBound && y >= 0;
 }
-void AddIfNumberAndSetItToX(int x, int y){
+void AddIfNumberAndSetItToX(int x, int y, ref int currentGearCount, ref int gearRatio){
     if(engineArray[x, y] == 'X' || engineArray[x, y] == '.' || symbolDictionary.Contains(engineArray[x, y])){
         return;
     }
+
+    currentGearCount++;
+
     int leftMost = x;
     int rightMost = x;
     GetLeftMostDigitIndex(x, y, ref leftMost);
     GetRightMostDigitIndex(x, y, ref rightMost);
 
+    int num = 0;
     if (leftMost == rightMost){
-        partNumberSum += Convert.ToInt32($"{engineArray[x, y]}");
+        num = Convert.ToInt32($"{engineArray[x, y]}");
         engineArray[x, y] = 'X';
+        gearRatio *= num;
         return;
     }
 
@@ -97,10 +107,12 @@ void AddIfNumberAndSetItToX(int x, int y){
         tempNum += engineArray[i, y];
         engineArray[i, y] = 'X';
     }
-    partNumberSum += Convert.ToInt32(tempNum);
+    num = Convert.ToInt32(tempNum);
+    gearRatio *= num;
 
 
 }
+
 
 void GetLeftMostDigitIndex(int x, int y, ref int leftMost){
     if (char.IsDigit(engineArray[x, y])){
